213f5ecdd287df6cb7527d1b68be65e362132897
Samuel Casto <Samuel.Casto@ibm.com>
2016-08-10 13:59:00 -0400

    Add editChar() and clearEditor()

--- EON/public/views/EditView/EditViewController.js
+++ EON/public/views/EditView/EditViewController.js
@@ -1,3 +1,53 @@
-angular.module("EditView").controller("EditViewController", function($scope, EditViewService){
+angular.module("EditView").controller("EditViewController", function(EditViewService) {
+    var vm = this;
+
+    vm.characters = [{
+        name: 'Reaper',
+        role: 'dps',
+        counters: 'tanks',
+        counteredBy: 'noone knows'
+    }, {
+        name: 'Tracer',
+        role: 'disruptor',
+        counters: 'backlines',
+        counteredBy: 'winston'
+    }, {
+        name: 'Genji',
+        role: 'disruptor',
+        counters: 'my sanity',
+        counteredBy: 'bad players'
+    }, {
+        name: 'Lucio',
+        role: 'support',
+        counters: 'damage',
+        counterdBy: 'autotarget'
+    }];
+    vm.editor = {};
+    vm.sendAlert = function(stuff, stuffTwo) {
+        alert(stuff + stuffTwo);
+
+    }
+
+    vm.addChar = function() {
+        vm.characters.push(charFactory(vm.editor.name, vm.editor.role, vm.editor.counters, vm.editor.counteredBy));
+        clearEditor();
+    }
+
+
+    function clearEditor() {
+        vm.editor = {};
+    }
+
+    function charFactory(charName, charRole, charCounters, charCounteredBy) {
+        return {
+            name: charName,
+            role: charRole,
+            counters: charCounters,
+            counteredBy: charCounteredBy
+        };
+    }
+
+
+
 
 });
