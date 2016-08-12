c12c2f317fabda3414ab42caed444cffccf8b5f1
Samuel Casto <Samuel.Casto@ibm.com>
2016-08-12 09:09:55 -0400

    Added the first of the database calls in my node routing.

--- EON/noderoutes.js
+++ EON/noderoutes.js
@@ -1,9 +1,32 @@
 var express = require("express");
 var router = express.Router();
+var request = require('request')
 
 module.exports = router;
 
-router.get("/eonEdit", function(req, res){
-  console.log("We got to the get");
-  res.send("A thing");
+router.get("/eonEdit", function(req, res) {
+    console.log("We got to the get");
+
+
+
+    var url = 'http://127.0.0.1:5984/'
+    var db = 'eonstorage/'
+    var id = 'document_id'
+
+    // Add a document with an ID
+    request.put({
+        url: url + db + id,
+        body: {
+            message: 'This is Sams new shiny document',
+            user: 'DeezNutz'
+        },
+        json: true,
+    }, function(err, resp, body) {
+        // Read the document
+        request(url + db + id, function(err, res, body) {
+            console.log(body.user + ' : ' + body.message)
+        })
+    })
+
+    res.send("A thing");
 });
